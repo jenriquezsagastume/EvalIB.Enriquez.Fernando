ccbed17fb8d6922fd2e01a1f99e1eb42e24a6d03
jenriquezsagastume <61293935+jenriquezsagastume@users.noreply.github.com>
2020-03-12 15:01:35 -0600

    Add files via upload

---
+++ documento.py
@@ -0,0 +1,10 @@
+mayor = 0
+maximo = 3
+for i in range(maximo);
+     num = int(input('seleccione 3 numero:'))
+     if num > mayor:
+         mayor = num
+print("el numero mayor es")
+print(mayor)
+input();
+ 
