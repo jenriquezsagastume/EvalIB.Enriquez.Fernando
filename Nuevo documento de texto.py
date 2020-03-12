dbd63134ba393e7af038cec1a5aae0f4dde51c29
jenriquezsagastume <61293935+jenriquezsagastume@users.noreply.github.com>
2020-03-12 14:41:55 -0600

    Add files via upload

---
+++ Nuevo documento de texto.py
@@ -0,0 +1,9 @@
+numero1=int(input("ingrese la primer nota\n"));
+numero2=int(input("ingrese la segunda nota\n"));
+numero3=int(input("ingrese la tercer nota\n"));
+numero4=int(input("ingrese la cuarta nota\n"));
+
+numero=(numero1+numero2+numero3+numero4)/4
+
+print("Tu promedio es")
+print(numero)
